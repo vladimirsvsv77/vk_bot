aa1d1ac3d3341336120455c32058fc7c59572e1c
vladimirsvsv77 <vladimirsvsv77@gmail.com>
2018-07-23 21:40:02 +0300

    added script logic

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@
 import requests
 import argparse
 import re
+import redis
 
 import vk_api
 from vk_api import VkUpload
@@ -14,6 +15,15 @@ parser = argparse.ArgumentParser(description='bot.py')
 parser.add_argument('-token')
 opt = parser.parse_args()
 token = opt.token
+
+
+sentences = ['Добрый день! Меня зовут Вера, подскажите, пожалуйста, находитесь ли вы в поиске работы? Ответьте, пожалуйста, да или нет.', 
+'Мы предлагаем официальное трудоустройство, уровень заработной платы от 100 000 рублей. Офис в центре Москвы, дружный коллектив. Скажите, интересна ли вам эта вакансия? Ответьте, пожалуйста, да или нет.',
+'Благодарю за ответы, в ближайшее время с вами свяжется наш менеджер по подбору персонала']
+negative = 'Извините, пожалуйста, за беспокойство. Хорошего дня!'
+
+
+r_server = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 
 class MLStripper(HTMLParser):
@@ -44,6 +54,21 @@ def get_answer_from_mailru(question):
     return answer
 
 
+def get_next_sentence(user_id, text):
+    if 'да' in text:
+        sentence_index = int(r_server.get(user_id))
+        text = sentences[sentence_index]
+        if sentence_index == len(sentence_index - 1):
+            r_server.set(user_id, 0)
+        else:
+            r_server.set(user_id, sentence_index + 1)
+    elif 'нет' in text:
+        text = negative
+        r_server.set(user_id, 0)
+    else:
+        text = "Извините, я не поняла ответ. Ответьте, пожалуйста, да или нет."
+
+
 def main():
     session = requests.Session()
     vk_session = vk_api.VkApi(token=token)
@@ -55,7 +80,12 @@ def main():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
             print('id{}: "{}"'.format(event.user_id, event.text), end=' ')
 
-            text = get_answer_from_mailru(event.text)
+            # text = get_answer_from_mailru(event.text)
+            if r_server[event.user_id] is None:
+                r_server.set(event.user_id, 0)
+                text = sentences[0]
+            else:
+                get_next_sentence(event.user_id, event.text.strip())
 
             if not text:
                 vk.messages.send(
